2b84a432560ccf385e96fd9e0a05fef31576d6d2
kota muto610 <mutoukouta0609@gmail>
2023-07-09 21:04:19 +0900

    0709(2)

--- share_office-vue/src/App.vue
+++ share_office-vue/src/App.vue
@@ -1,17 +1,11 @@
 <template>
-  <LoginPage />
+  <div>ホームです</div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/login">Login</router-link> 
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import LoginPage from './components/LoginFunction/LoginPage.vue'
-
-export default {
-  name: 'App',
-  components: {
-    LoginPage
-  }
-}
-</script>
 
 <style>
 #app {
@@ -20,7 +14,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  font-size:30px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
